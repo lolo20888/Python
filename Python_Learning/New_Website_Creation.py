974996e2dfa8b6a1d686b7a08ef3d9c18769964d
Mina Remon <mina200888@icloud.com>
2018-09-22 14:30:42 +0200

    new file:   New_Website_Creation.py

---
+++ Python_Learning/New_Website_Creation.py
@@ -0,0 +1,11 @@
+import media
+from media import Movie
+import fresh_tomatoes
+toy_story = media.Movie("Toy Story",
+                        "Led by Woody, Andy's toys live happily in his room until Andy's birthday brings Buzz Lightyear onto the scene. Afraid of losing his place in Andy's heart, Woody plots against Buzz. But when circumstances separate Buzz and Woody from their owner, the duo eventually learns to put aside their differences.",
+                        "https://www.google.com.eg/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjjytTQqsLdAhXQC-wKHQbuDVQQjRx6BAgBEAU&url=http%3A%2F%2Fwww.impawards.com%2F1995%2Ftoy_story_ver1.html&psig=AOvVaw0b60chDDR1JdpdanhSHEBY&ust=1537283814128136",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+avatar =media.Movie("Avatar",
+                    "On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora. Jake Sully (Sam Worthington), a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na'vi woman (Zoe Saldana). As a bond with her grows, he is drawn into a battle for the survival of her world.",
+                    "https://www.google.com.eg/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjU46q3sMLdAhUqsaQKHV08BeAQjRx6BAgBEAU&url=https%3A%2F%2Fsco.wikipedia.org%2Fwiki%2FFile%3AAvatar-Teaser-Poster.jpg&psig=AOvVaw3Dy9eHBKnTO64U7rbGyZsc&ust=1537285374486113",
+                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
