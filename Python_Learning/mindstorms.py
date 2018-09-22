bd72e126358f5aaddea4e67c9bf0ee4dc2456b3b
Mina Remon <mina200888@icloud.com>
2018-09-22 15:20:51 +0200

    new file:   mindstorms.py

---
+++ Python_Learning/mindstorms.py
@@ -0,0 +1,24 @@
+import turtle
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("circle")
+    brad.color("green")
+    brad.speed(3)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+    angy = turtle.Turtle()
+    angy.circle(100)
+    angy.shape("square")
+    angy.color("blue")
+    window.exitonclick()
+draw_square()
